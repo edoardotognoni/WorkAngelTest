3190d119cc40bb7dcd19fee515baf5e347f38c31
unknown <edoardotognoni@gmail.com>
2015-03-04 23:36:22 +0100

    call, mail, sms as options menu

--- app/src/main/java/com/workangel/tech/test/FragmentEmployeeDetail.java
+++ app/src/main/java/com/workangel/tech/test/FragmentEmployeeDetail.java
@@ -1,11 +1,12 @@
 package com.workangel.tech.test;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.text.TextUtils;
+import android.view.*;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.koushikdutta.ion.Ion;
@@ -36,13 +37,66 @@ public class FragmentEmployeeDetail extends Fragment {
         ImageView employeeAvatar = (ImageView) root.findViewById(R.id.employee_avatar);
         TextView employeeName = (TextView) root.findViewById(R.id.employee_name);
         TextView employeeDepartment = (TextView) root.findViewById(R.id.employee_department);
+        TextView employeeAbout = (TextView) root.findViewById(R.id.employee_about);
 
         Ion.with(employeeAvatar).load(mEmployee.getAvatar());
         employeeName.setText(mEmployee.getLastName() + " " + mEmployee.getFirstName());
         employeeDepartment.setText(mEmployee.getDepartment());
+        employeeAbout.setText(mEmployee.getAbout());
+
+        setHasOptionsMenu(true);
         return root;
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_employee_detail,menu);
+        //If employee doesn't have email address,or phone number, make menu items invisible
+        if (TextUtils.isEmpty(mEmployee.getEmail())) {
+            menu.findItem(R.id.action_send_email).setVisible(false);
+        }
+
+        if (TextUtils.isEmpty(mEmployee.getPhone())) {
+            menu.findItem(R.id.action_call).setVisible(false);
+            menu.findItem(R.id.action_send_sms).setVisible(false);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_send_email:
+                sendEmail(mEmployee.getEmail());
+                break;
+            case R.id.action_send_sms:
+                sendSms(mEmployee.getPhone());
+                break;
+            case R.id.action_call:
+                call(mEmployee.getPhone());
+                break;
+        }
+        return true;
+    }
+
+    private void sendEmail(String address) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto",address, null));
+        intent.putExtra(Intent.EXTRA_EMAIL, address);
+        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.contact_request));
+
+        startActivity(Intent.createChooser(intent, getString(R.string.send_mail)));
+    }
+
+    private void sendSms(String phone) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.fromParts("sms", phone, null));
+        startActivity(intent);
+    }
+
+    private void call(String phone) {
+        String uri = "tel:" + phone.trim() ;
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse(uri));
+        startActivity(intent);
+    }
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
