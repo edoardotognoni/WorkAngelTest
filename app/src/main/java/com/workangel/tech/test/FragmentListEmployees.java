653a997cdb05f1ecb8d7fec06bc568300a279437
unknown <edoardotognoni@gmail.com>
2015-03-05 21:53:41 +0100

    better fragment management-API call is made only at startup from the Activity

--- app/src/main/java/com/workangel/tech/test/FragmentListEmployees.java
+++ app/src/main/java/com/workangel/tech/test/FragmentListEmployees.java
@@ -1,34 +1,22 @@
 package com.workangel.tech.test;
 
-import android.app.AlertDialog;
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.*;
-import com.workangel.tech.test.contacts.ContactsSaverIntentService;
-import com.workangel.tech.test.database.FactoryDatabaseInterface;
-import com.workangel.tech.test.database.FactoryDatabaseManager;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Spinner;
 import com.workangel.tech.test.database.bean.Employee;
 import com.workangel.tech.test.hierarchy.CompanyHierarchyTree;
 import com.workangel.tech.test.hierarchy.Node;
-import com.workangel.tech.test.network.Constants;
-import com.workangel.tech.test.network.FactoryNetworkManager;
-import com.workangel.tech.test.network.FactoryNetworkManagerInterface;
-import com.workangel.tech.test.network.ResultCallback;
 import de.greenrobot.event.EventBus;
-import de.keyboardsurfer.android.widget.crouton.Crouton;
-import de.keyboardsurfer.android.widget.crouton.Style;
 
 import java.util.*;
 
@@ -41,9 +29,11 @@ public class FragmentListEmployees extends Fragment implements LoaderManager.Loa
     private ListView mEmployeesListView;
     private int mDepartmentSpinnerSelection = 0;
     private String mQueryName = "";
-
+    /**
+     * Loader constant for the LoaderManager
+     */
     public static final int EMPLOYEES_LOADER_ID = 1000;
-    public static final String KEY_DOWNLOADED_ONCE = "key_downloaded_once";
+
     /**
      * Keys used to retain the list on orientation changes
      */
@@ -64,7 +54,6 @@ public class FragmentListEmployees extends Fragment implements LoaderManager.Loa
      * This object represents the company's hierarchy tree.
      */
     private CompanyHierarchyTree mCompanyTree;
-    private ProgressDialog mLoadingDialog;
 
     @Override
     public View onCreateView(LayoutInflater inflater,
@@ -75,11 +64,22 @@ public class FragmentListEmployees extends Fragment implements LoaderManager.Loa
         mDepartmentSpinner = (Spinner) root.findViewById(R.id.department_spinner);
         mEmployeesSearch = (SearchView) root.findViewById(R.id.employees_search);
 
-        //Create a loading dialog for the first time we open the app
-        mLoadingDialog = new ProgressDialog(getActivity());
-        mLoadingDialog.setMessage(getString(R.string.loading));
-        mLoadingDialog.setCancelable(false);
+        mEmployeesList = new ArrayList<>();
+        if (getArguments() != null) {
+            mEmployeesList = getArguments().getParcelableArrayList(KEY_EMPLOYEES_LIST);
+        }
 
+        /**
+         * Set a click listener to move to the employee detail fragment
+         */
+        mEmployeesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Employee employee = (Employee) parent.getItemAtPosition(position);
+                Node treeNode = mCompanyTree.getNode(employee.get_id());
+                EventBus.getDefault().post(new MainActivity.EventTransactToEmployeeDetailFragment(employee,treeNode));
+            }
+        });
 
         mDepartmentSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             //When setOnItemSelectedListener is set, it auto calls onItemSelected at first. I don't want
@@ -101,7 +101,6 @@ public class FragmentListEmployees extends Fragment implements LoaderManager.Loa
 
             }
         });
-
         mEmployeesSearch.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -113,24 +112,13 @@ public class FragmentListEmployees extends Fragment implements LoaderManager.Loa
                 mQueryName = newText;
                 //After orientation changes this method is called before everything. We'll wait for
                 // the instance to be restored
-                if (mDepartmentEmployeesMap != null) {
+                if (mDepartmentEmployeesMap != null && mEmployeesListView.getAdapter() != null) {
                     filter();
                 }
                 return true;
             }
         });
 
-        /**
-         * Set a click listener to move to the employee detail fragment
-         */
-        mEmployeesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Employee employee = (Employee) parent.getItemAtPosition(position);
-                Node treeNode = mCompanyTree.getNode(employee.get_id());
-                EventBus.getDefault().post(new MainActivity.EventTransactToEmployeeDetailFragment(employee,treeNode));
-            }
-        });
         /**
          * If we come from a configuration change, don't make a new call. We don't need to
          * call the API every time we change from portrait to landscape and viceversa
@@ -139,69 +127,32 @@ public class FragmentListEmployees extends Fragment implements LoaderManager.Loa
             mEmployeesList = savedInstanceState.getParcelableArrayList(KEY_EMPLOYEES_LIST);
             mDepartmentSpinnerSelection = savedInstanceState.getInt(KEY_SPINNER_DEPT_SELECTION);
             mQueryName = savedInstanceState.getString(KEY_QUERY_NAME);
+            update(mEmployeesList);
         }
         else {
-            if (!UserDataManager.getDownloadedOnce(getActivity())) {
-                //We still don't have available data. Show a loading dialog until we get some
-                mLoadingDialog.show();
-            }
-
-            //Make the API call to get employees data
-            FactoryNetworkManagerInterface networkManagerInterface = FactoryNetworkManager.getInstance(getActivity(),
-                                                                                                       FactoryNetworkManager.NetworkFramework.RETROFIT);
-
-
-            networkManagerInterface.getAllEmployees(new ResultCallback<List<Employee>>() {
-
-                @Override
-                public void onSuccess(List<Employee> result) {
-                    Log.d(TAG, "Employees downloaded successfully");
-                    if (isAdded()) {
-                        //Save that we downloaded at least once
-                        UserDataManager.setDownloadedOnce(getActivity(), true);
-
-                        //Save contacts?
-                        if (!UserDataManager.getStopAskingForSavingContacts(getActivity())) {
-                            askToSaveContacts(result);
-                        }
-                        //Save emplyees to DB
-                        saveEmployeesAsync(result);
-                    }
-                }
-
-                @Override
-                public void onError(int errorCode) {
-                    if (isAdded()) {
-                        if (mLoadingDialog.isShowing()) {
-                            mLoadingDialog.dismiss();
-                        }
-
-                        switch (errorCode) {
-                            case Constants.CONVERSION_PROBLEM:
-                            case Constants.UNEXPECTED_ERROR:
-                                Crouton.makeText(getActivity(), getString(R.string.data_invalid_contact_server_admin),
-                                                 Style.ALERT).show();
-                                break;
-                            case Constants.NETWORK_PROBLEM:
-                                Crouton.makeText(getActivity(), getString(R.string.error_network_problem),
-                                                 Style.ALERT).show();
-                                break;
-                        }
-                    }
-                }
-            });
-
+            //Init loader
+            getLoaderManager().restartLoader(EMPLOYEES_LOADER_ID, null, this).forceLoad();
         }
 
-        /**
-         * While we wait for the API call to finish, we show old data from DB
-         */
-        getLoaderManager().initLoader(EMPLOYEES_LOADER_ID, null, this).forceLoad();
 
         return root;
     }
 
+    /**
+     * Updates UI and buil hierarchy tree and department map
+     * @param fullList
+     */
+    public void update(List<Employee> fullList) {
+        mEmployeesList = fullList;
+        buildDepartmentMap(mEmployeesList);
+        mCompanyTree = new CompanyHierarchyTree(mEmployeesList);
+        filter();
+    }
 
+
+    /**
+     * Filter the ListView with department chosen and query text
+     */
     private void filter() {
         //First filter for department
         String department = (String) mDepartmentSpinner.getItemAtPosition(mDepartmentSpinnerSelection);
@@ -254,7 +205,7 @@ public class FragmentListEmployees extends Fragment implements LoaderManager.Loa
         //If we come from a configuration change, we set the old values to the spinner and searchview
         //At first launch this set the selection to 0 and set an empty string. Sounds good to me.
         mDepartmentSpinner.setSelection(mDepartmentSpinnerSelection);
-        mEmployeesSearch.setQuery(mQueryName,false);
+        mEmployeesSearch.setQuery(mQueryName, false);
         //Show filter views
         mDepartmentSpinner.setVisibility(View.VISIBLE);
         mEmployeesSearch.setVisibility(View.VISIBLE);
@@ -291,79 +242,6 @@ public class FragmentListEmployees extends Fragment implements LoaderManager.Loa
         outState.putString(KEY_QUERY_NAME, mQueryName);
     }
 
-    /**
-     * Saves new employees on a background thread and restart the loader. It's important
-     * to make it run on a Bg thread otherwise it could freeze the UI. And we don't want this.
-     * @param employees Employees
-     */
-    private void saveEmployeesAsync(final List<Employee> employees) {
-        //Use AsyncTask because its onPostExecute method is useful in this case. It runs on the UI thread
-        // so we can easily update contents
-        new AsyncTask<Void,Void,Void>() {
-
-            @Override
-            protected Void doInBackground(Void... params) {
-                FactoryDatabaseInterface dbManager = FactoryDatabaseManager.getInstance(getActivity(),
-                                                                                        FactoryDatabaseManager
-                                                                                            .DatabaseFramework.ORMLITE);
-
-                dbManager.saveEmployees(employees);
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(Void aVoid) {
-                if (isAdded()) {
-                    //Update UI by restarting loader
-                    getLoaderManager().restartLoader(EMPLOYEES_LOADER_ID, null, FragmentListEmployees.this).forceLoad();
-                    if (mLoadingDialog.isShowing()) {
-                        mLoadingDialog.dismiss();
-                    }
-                }
-                super.onPostExecute(aVoid);
-            }
-        }.execute();
-
-    }
-
-    /**
-     * Shows a dialog to the user which will ask if he wants employees to be
-     * saved on its contacts
-     * @param employees Employees to be saved
-     */
-    private void askToSaveContacts(final List<Employee> employees) {
-        View confirmationDialogView = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_ask_save_contact, null);
-        final CheckBox dontAskAgain = (CheckBox) confirmationDialogView.findViewById(R.id.dont_ask_again);
-        AlertDialog confirmationDialog = new AlertDialog.Builder(getActivity())
-            .setView(confirmationDialogView)
-            .setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    if (dontAskAgain.isChecked()) {
-                        UserDataManager.setStopAskingForSavingContacts(getActivity(),true);
-                    }
-                    //Starts service which will save contacts
-                    Intent intent = new Intent(getActivity(), ContactsSaverIntentService.class);
-                    intent.putParcelableArrayListExtra(ContactsSaverIntentService.KEY_EMPLOYEES_LIST,
-                                                       (ArrayList<? extends android.os.Parcelable>) employees);
-                    getActivity().startService(intent);
-                }
-            })
-            .setNegativeButton(getString(R.string.no), new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    if (dontAskAgain.isChecked()) {
-                        UserDataManager.setStopAskingForSavingContacts(getActivity(),true);
-                    }
-                }
-            })
-            .create();
-
-        confirmationDialog.show();
-
-    }
-
-
     @Override
     public Loader<List<Employee>> onCreateLoader(int id, Bundle args) {
         return new EmployeesLoader(getActivity());
@@ -371,20 +249,12 @@ public class FragmentListEmployees extends Fragment implements LoaderManager.Loa
 
     @Override
     public void onLoadFinished(Loader<List<Employee>> loader, List<Employee> data) {
-        mEmployeesList = data;
-        if (isAdded()) {
-            //Build map Department-Employees
-            buildDepartmentMap(mEmployeesList);
-            mCompanyTree = new CompanyHierarchyTree(mEmployeesList);
-            //Filter for department and name if something was set before
-            filter();
-        }
+        //Update Employees list
+        update(data);
     }
 
     @Override
     public void onLoaderReset(Loader<List<Employee>> loader) {
-        mEmployeesListView.setAdapter(null);
+
     }
-
-
 }
--- app/src/main/java/com/workangel/tech/test/MainActivity.java
+++ app/src/main/java/com/workangel/tech/test/MainActivity.java
@@ -1,19 +1,43 @@
 package com.workangel.tech.test;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.CheckBox;
+import com.workangel.tech.test.contacts.ContactsSaverIntentService;
+import com.workangel.tech.test.database.FactoryDatabaseInterface;
+import com.workangel.tech.test.database.FactoryDatabaseManager;
 import com.workangel.tech.test.database.bean.Employee;
 import com.workangel.tech.test.hierarchy.Node;
+import com.workangel.tech.test.network.Constants;
+import com.workangel.tech.test.network.FactoryNetworkManager;
+import com.workangel.tech.test.network.FactoryNetworkManagerInterface;
+import com.workangel.tech.test.network.ResultCallback;
 import de.greenrobot.event.EventBus;
+import de.keyboardsurfer.android.widget.crouton.Crouton;
+import de.keyboardsurfer.android.widget.crouton.Style;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity implements FragmentManager.OnBackStackChangedListener {
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    public static final String KEY_DOWNLOADED_ONCE = "key_downloaded_once";
+    private ProgressDialog mLoadingDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,19 +58,146 @@ public class MainActivity extends ActionBarActivity implements FragmentManager.O
         getSupportActionBar().setIcon(R.drawable.toolbar_icon);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
+        //Create a loading dialog for the first time we open the app
+        mLoadingDialog = new ProgressDialog(this);
+        mLoadingDialog.setMessage(getString(R.string.loading));
+        mLoadingDialog.setCancelable(false);
+
         //Check if fragment has to be recreated or not
-        Fragment fragment =  getSupportFragmentManager().findFragmentById(
-            R.id.fragment_container);
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
 
         if (fragment == null) {
+            FragmentListEmployees fragmentListEmployees = new FragmentListEmployees();
             FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container, new FragmentListEmployees());
+            fragmentTransaction.replace(R.id.fragment_container, fragmentListEmployees);
             fragmentTransaction.commit();
+
+            if (!UserDataManager.getDownloadedOnce(this)) {
+                //We still don't have available data. Show a loading dialog until we get some
+                mLoadingDialog.show();
+            }
+
+            //Make the API call to get employees data
+            FactoryNetworkManagerInterface networkManagerInterface = FactoryNetworkManager.getInstance(this,
+                                                                                                       FactoryNetworkManager.NetworkFramework.RETROFIT);
+
+
+            networkManagerInterface.getAllEmployees(new ResultCallback<List<Employee>>() {
+
+                @Override
+                public void onSuccess(List<Employee> result) {
+                    Log.d(TAG, "Employees downloaded successfully");
+                    //Save that we downloaded at least once
+                    UserDataManager.setDownloadedOnce(MainActivity.this, true);
+
+                    //Save contacts?
+                    if (!UserDataManager.getStopAskingForSavingContacts(MainActivity.this)) {
+                        askToSaveContacts(result);
+                    }
+                    //Save employees to DB
+                    saveEmployeesAsync(result);
+                }
+
+                @Override
+                public void onError(int errorCode) {
+                    if (mLoadingDialog.isShowing()) {
+                        mLoadingDialog.dismiss();
+                    }
+
+                    switch (errorCode) {
+                        case Constants.CONVERSION_PROBLEM:
+                        case Constants.UNEXPECTED_ERROR:
+                            Crouton.makeText(MainActivity.this, getString(R.string.data_invalid_contact_server_admin),
+                                             Style.ALERT).show();
+                            break;
+                        case Constants.NETWORK_PROBLEM:
+                            Crouton.makeText(MainActivity.this, getString(R.string.error_network_problem),
+                                             Style.ALERT).show();
+                            break;
+                    }
+                }
+            });
         }
 
         getSupportFragmentManager().addOnBackStackChangedListener(this);
         //After orientation change, check how many fragments we have
         onBackStackChanged();
+    }
+
+    /**
+     * Saves new employees on a background thread and restart the loader. It's important
+     * to make it run on a Bg thread otherwise it could freeze the UI. And we don't want this.
+     *
+     * @param employees Employees
+     */
+    private void saveEmployeesAsync(final List<Employee> employees) {
+        //Use AsyncTask because its onPostExecute method is useful in this case. It runs on the UI thread
+        // so we can easily update contents
+        new AsyncTask<Void, Void, Void>() {
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                FactoryDatabaseInterface dbManager = FactoryDatabaseManager.getInstance(MainActivity.this,
+                                                                                        FactoryDatabaseManager
+                                                                                            .DatabaseFramework.ORMLITE);
+
+                dbManager.saveEmployees(employees);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                //Update FragmentListEmployees if it's in the top, otherwise it will load
+                //new data from the DB by itself
+                Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+                if (fragment instanceof FragmentListEmployees) {
+                    ((FragmentListEmployees) fragment).update(employees);
+                }
+                if (mLoadingDialog.isShowing()) {
+                    mLoadingDialog.dismiss();
+                }
+                super.onPostExecute(aVoid);
+            }
+        }.execute();
+
+    }
+
+    /**
+     * Shows a dialog to the user which will ask if he wants employees to be
+     * saved on its contacts
+     *
+     * @param employees Employees to be saved
+     */
+    private void askToSaveContacts(final List<Employee> employees) {
+        View confirmationDialogView = LayoutInflater.from(this).inflate(R.layout.dialog_ask_save_contact, null);
+        final CheckBox dontAskAgain = (CheckBox) confirmationDialogView.findViewById(R.id.dont_ask_again);
+        AlertDialog confirmationDialog = new AlertDialog.Builder(this)
+            .setView(confirmationDialogView)
+            .setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    if (dontAskAgain.isChecked()) {
+                        UserDataManager.setStopAskingForSavingContacts(MainActivity.this, true);
+                    }
+                    //Starts service which will save contacts
+                    Intent intent = new Intent(MainActivity.this, ContactsSaverIntentService.class);
+                    intent.putParcelableArrayListExtra(ContactsSaverIntentService.KEY_EMPLOYEES_LIST,
+                                                       (ArrayList<? extends Parcelable>) employees);
+                    startService(intent);
+                }
+            })
+            .setNegativeButton(getString(R.string.no), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    if (dontAskAgain.isChecked()) {
+                        UserDataManager.setStopAskingForSavingContacts(MainActivity.this, true);
+                    }
+                }
+            })
+            .create();
+
+        confirmationDialog.show();
+
     }
 
     @Override
@@ -65,7 +216,9 @@ public class MainActivity extends ActionBarActivity implements FragmentManager.O
 
 
     /**
-     * Called by EventBus when someone fires an {@link com.workangel.tech.test.MainActivity.EventTransactToEmployeeDetailFragment}
+     * Called by EventBus when someone fires an {@link com.workangel.tech.test.MainActivity
+     * .EventTransactToEmployeeDetailFragment}
+     *
      * @param eventTransactToEmployeeDetailFragment Event
      */
     public void onEvent(EventTransactToEmployeeDetailFragment eventTransactToEmployeeDetailFragment) {
@@ -75,13 +228,15 @@ public class MainActivity extends ActionBarActivity implements FragmentManager.O
         bundle.putParcelable(FragmentEmployeeDetail.KEY_NODE, eventTransactToEmployeeDetailFragment.getNode());
         employeeDetail.setArguments(bundle);
         getSupportFragmentManager().beginTransaction()
-                                   .add(R.id.fragment_container, employeeDetail)
+                                   .replace(R.id.fragment_container, employeeDetail)
                                    .addToBackStack(null)
                                    .commit();
     }
 
     /**
-     * Called by EventBus when someone fires an {@link com.workangel.tech.test.MainActivity.EventTransactToSubordinateOrBossFragment}
+     * Called by EventBus when someone fires an {@link com.workangel.tech.test.MainActivity
+     * .EventTransactToSubordinateOrBossFragment}
+     *
      * @param eventTransactToEmployeeDetailFragment Event
      */
     public void onEvent(EventTransactToSubordinateOrBossFragment eventTransactToEmployeeDetailFragment) {
@@ -91,7 +246,7 @@ public class MainActivity extends ActionBarActivity implements FragmentManager.O
         bundle.putParcelable(FragmentEmployeeDetail.KEY_NODE, eventTransactToEmployeeDetailFragment.getNode());
         employeeDetail.setArguments(bundle);
         getSupportFragmentManager().beginTransaction()
-                                   .add(R.id.fragment_container, employeeDetail)
+                                   .replace(R.id.fragment_container, employeeDetail)
                                    .addToBackStack(null)
                                    .commit();
     }
@@ -122,8 +277,9 @@ public class MainActivity extends ActionBarActivity implements FragmentManager.O
 
         /**
          * Constructor
+         *
          * @param employee Employee to be displayed
-         * @param node Node representing the Employee in the Hierarchy tree
+         * @param node     Node representing the Employee in the Hierarchy tree
          */
         public EventTransactToEmployeeDetailFragment(Employee employee, Node node) {
             mEmployee = employee;
@@ -153,8 +309,9 @@ public class MainActivity extends ActionBarActivity implements FragmentManager.O
 
         /**
          * Constructor
+         *
          * @param employee Employee to be displayed
-         * @param node Node representing the Employee in the Hierarchy tree
+         * @param node     Node representing the Employee in the Hierarchy tree
          */
 
         public EventTransactToSubordinateOrBossFragment(Employee employee, Node node) {
